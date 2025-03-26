ae5f940ba68a8d94bb089c489e794c4f30258b89
Gabriel Porto <7gabrielporto@gmail.com>
2025-03-26 20:50:13 -0300

    feat: add sampleget spreadsheet data route

---
+++ src/pages/api/get-spreadsheet-data.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { sheets, SPREADSHEET_ID } from "../../services/google/googleSheets";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Método não permitido" });
+  }
+
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET_ID,
+      range: "A1:D8",
+    });
+
+    return res.status(200).json({ data: response.data.values });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+}
