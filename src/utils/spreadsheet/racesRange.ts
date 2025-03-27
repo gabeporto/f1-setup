3705a86274b08a3706bceaece3a9602ab38c7317
Gabriel Porto <7gabrielporto@gmail.com>
2025-03-27 18:23:51 -0300

    feat: add races spreadsheet range

---
+++ src/utils/spreadsheet/racesRange.ts
@@ -0,0 +1,17 @@
+export const RACE_WITHOUT_SPRINT_RANGE = {
+    fp1: "!B23:D42",
+    fp2: "!G23:I42",
+    fp3: "!L23:N42",
+    qualy: "!Q23:S42",
+    race: "!V23:X42",
+    partialRace: "!W6:W18"
+}
+export const RACE_WITH_SPRINT_RANGE = {
+    fp1: "!B23:D42",
+    sprintQualy: "!G23:I42",
+    sprint: "!L23:N42",
+    qualy: "!Q23:S42",
+    race: "!V23:X42",
+    partialRace: "!W6:W18",
+    partialSprint: "!AA23:AA42"
+}
