05f210a3149e643a5e692661800140d8e8fbc010
Gabriel Porto <7gabrielporto@gmail.com>
2025-03-28 18:13:32 -0300

    feat: update race spreadsheet ranges

--- src/utils/spreadsheet/racesRange.ts
+++ src/utils/spreadsheet/racesRange.ts
@@ -1,17 +1,17 @@
 export const RACE_WITHOUT_SPRINT_RANGE = {
-    fp1: "!B24:D43",
-    fp2: "!G24:I43",
-    fp3: "!L24:N43",
-    qualy: "!Q24:S43",
-    race: "!V24:X43",
-    partialRace: "!W6:W18"
+    fp1: "!C24:E43",
+    fp2: "!H24:J43",
+    fp3: "!M24:O43",
+    qualy: "!R24:T43",
+    race: "!W24:Y43",
+    partialRace: "!X6:X18"
 }
 export const RACE_WITH_SPRINT_RANGE = {
-    fp1: "!B24:D43",
-    sprintQualy: "!G24:I43",
-    sprint: "!L24:N43",
-    qualy: "!Q24:S43",
-    race: "!V24:X43",
-    partialRace: "!W6:W18",
-    partialSprint: "!AA6:AA18"
+    fp1: "!C24:E43",
+    sprintQualy: "!H24:J43",
+    sprint: "!M24:O43",
+    qualy: "!R24:T43",
+    race: "!W24:Y43",
+    partialRace: "!X6:X18",
+    partialSprint: "!AB6:AB18"
 }
